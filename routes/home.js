784bc4b6ae5c655fff512aae244af8d163cc4c48
Shrutiloya <shrutiloya1@gmail.com>
2017-05-11 17:15:27 -0700

    Project setup

---
+++ routes/home.js
@@ -0,0 +1,137 @@
+var ejs = require("ejs");
+var mysql = require('./mysql');
+
+
+function signin(req,res) {
+
+	ejs.renderFile('./views/index.ejs',function(err, result) {
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+
+function afterSignIn(req,res)
+{
+	// check user already exists
+	var getUser="select * from users where username='"+req.param("inputUsername")+"' and password='" + req.param("inputPassword") +"'";
+	console.log("Query is:"+getUser);
+	
+	mysql.fetchData(function(err,results){
+		if(err){
+			throw err;
+		}
+		else 
+		{
+			if(results.length > 0){
+				console.log("valid Login");
+				res.render("Userhomepage");
+			}
+			else {    
+				
+				console.log("Invalid Login");
+				ejs.renderFile('./views/failLogin.ejs',function(err, result) {
+			        // render on success
+			        if (!err) {
+			            res.end(result);
+			        }
+			        // render or error
+			        else {
+			            res.end('An error occurred');
+			            console.log(err);
+			        }
+			    });
+			}
+		}  
+	},getUser);
+}
+
+function getAllUsers(req,res)
+{
+	var getAllUsers = "select * from users";
+	console.log("Query is:"+getAllUsers);
+	
+	mysql.fetchData(function(err,results){
+		if(err){
+			throw err;
+		}
+		else 
+		{
+			if(results.length > 0){
+				
+				var rows = results;
+				var jsonString = JSON.stringify(results);
+				var jsonParse = JSON.parse(jsonString);
+
+				ejs.renderFile('./views/successLogin.ejs',{data:jsonParse},function(err, result) {
+			        // render on success
+			        if (!err) {
+			            res.end(result);
+			        }
+			        // render or error
+			        else {
+			            res.end('An error occurred');
+			            console.log(err);
+			        }
+			    });
+			}
+			else {    
+				
+				console.log("No users found in database");
+				ejs.renderFile('./views/failLogin.ejs',function(err, result) {
+			        // render on success
+			        if (!err) {
+			            res.end(result);
+			        }
+			        // render or error
+			        else {
+			            res.end('An error occurred');
+			            console.log(err);
+			        }
+			    });
+			}
+		}  
+	},getAllUsers);
+}
+
+
+function afterRegister(req,res)
+{
+	var json_responses;
+	var registerUser="insert into users(username,password,firstname,lastname,usertype) values('"+req.param("username")+"','"+req.param("password")+"','"+req.param("firstname")+"','"+req.param("lastname")+"',2)";
+	mysql.fetchData(function(err,results){
+		if(err){
+
+			json_responses = {"statusCode" : 401};
+			res.send(json_responses);
+		}
+		else {
+
+			res.render("index");
+
+
+		}
+	},registerUser);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+exports.afterRegister=afterRegister;
+exports.signin=signin;
+exports.afterSignIn=afterSignIn;
+exports.getAllUsers=getAllUsers;
---
+++ routes/mysql.js
@@ -0,0 +1,38 @@
+var ejs= require('ejs');
+var mysql = require('mysql');
+
+//Put your mysql configuration settings - user, password, database and port
+function getConnection(){
+	var connection = mysql.createConnection({
+	    host     : 'communityapp.csa7tdbaawn0.us-west-2.rds.amazonaws.com',
+	    user     : 'team22',
+	    password : 'password',
+	    database : 'Community',
+	    port	 : 3306,
+		debug 	 : true
+	});
+	return connection;
+}
+
+
+function fetchData(callback,sqlQuery){
+	
+	console.log("\nSQL Query::"+sqlQuery);
+	
+	var connection=getConnection();
+	
+	connection.query(sqlQuery, function(err, rows, fields) {
+		if(err){
+			console.log("ERROR: " + err.message);
+		}
+		else 
+		{	// return err or result
+			console.log("DB Results:"+rows);
+			callback(err, rows);
+		}
+	});
+	console.log("\nConnection closed..");
+	connection.end();
+}	
+
+exports.fetchData=fetchData;
