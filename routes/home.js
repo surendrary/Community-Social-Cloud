47b44a9f3aafff1c56567c866cd92771cb0a6934
Amay Dubey <amaydubey@gmail.com>
2017-05-16 13:11:23 -0700

    One more temp change on same line
    Will remove after testing

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ var mysql = require('./mysql');
 
 function signin(req,res) {
 
-	ejs.renderFile('views/index.ejs',function(err, result) {
+	ejs.renderFile('./CommunitySocialCloud/views/index.ejs',function(err, result) {
 	   if (!err) {
 	            res.end(result);
 	   }
